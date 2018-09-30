3c7661415641c79922b235f00acfd75283599a73
DESKTOP-QB85EGP\jesse <0945181@hr.nl>
2018-09-30 18:28:05 +0200

    no message

--- ProjectC/Controllers/UserController.cs
+++ ProjectC/Controllers/UserController.cs
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-=======
-﻿using DevOne.Security.Cryptography.BCrypt;
->>>>>>> 8055590a6a5376db610bd92c023726db1a3bb2f6
-=======
-﻿using DevOne.Security.Cryptography.BCrypt;
->>>>>>> 000333b2f7e84255db73b50a9d73d2f5431b85a4
+using DevOne.Security.Cryptography.BCrypt;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
@@ -33,7 +21,7 @@ namespace ProjectC.Controllers
             var json = JsonConvert.SerializeObject(users);
             return json;
         }
-        
+
         //Get 1 user with the given id
         [HttpGet("{id}")]
         public string Get(int id)
@@ -46,20 +34,6 @@ namespace ProjectC.Controllers
 
         //Login a user with the given credentials
         [HttpPost]
-<<<<<<< HEAD
-<<<<<<< HEAD
-        public void Post([FromBody] string value)
-        {
-            var daoManager = HttpContext.RequestServices.GetService<DaoManager>();
-            var user = JsonConvert.DeserializeObject<User>(value);
-            daoManager?.UserDao.Save(user);
-        }
-
-        [HttpPost]
-        public void Register(string value)
-=======
-=======
->>>>>>> 000333b2f7e84255db73b50a9d73d2f5431b85a4
         public bool Login([FromBody] UserLoginModel input)
         {
             var daoManager = HttpContext.RequestServices.GetService<DaoManager>();
@@ -77,16 +51,12 @@ namespace ProjectC.Controllers
         //Register a user with the given register info
         [HttpPost]
         public void Register([FromBody] UserRegisterModel input)
-<<<<<<< HEAD
->>>>>>> 8055590a6a5376db610bd92c023726db1a3bb2f6
-=======
->>>>>>> 000333b2f7e84255db73b50a9d73d2f5431b85a4
         {
             var daoManager = HttpContext.RequestServices.GetService<DaoManager>();
             var user = new User(input);
             daoManager?.UserDao.Save(user);
         }
-        
+
         //Update the given user
         [HttpPut("{id}")]
         public void Update(int id, [FromBody] UserUpdateModel input)
