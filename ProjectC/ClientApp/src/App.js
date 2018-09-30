666ab8acd91c92d679d99a67010cf6df8b667225
Unknown <charlottewaldschmit@gmail.com>
2018-09-30 21:36:43 +0200

    beau gaat dit ff proberen

--- ProjectC/ClientApp/src/App.js
+++ ProjectC/ClientApp/src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router';
 
 import productPage from './components/productPage';
 import singleProduct from './components/singleProduct';
+import loginPage from './components/loginPage';
 
 export default class App extends Component {
     render() {
@@ -10,6 +11,7 @@ export default class App extends Component {
             <div className="App">
                 <Route exact path="/products" component={productPage} />
                 <Route path="/product/" component={singleProduct} />
+                <Route path="/product/" component={loginPage}/>
             </div>
         );
     }
