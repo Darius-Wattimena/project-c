e927b5502dd7abc0f093f3eb254eaee4175eadf9
6bq <31695264+6bq@users.noreply.github.com>
2018-10-08 00:51:48 +0200

    Shopping cart functionality

--- ProjectC/ClientApp/src/ShoppingCart/ShoppingCart.jsx
+++ ProjectC/ClientApp/src/ShoppingCart/ShoppingCart.jsx
@@ -6,15 +6,6 @@ import { history } from '../_helpers';
 
 import '../styling/ShoppingCartListingStyle.css';
 
-function getIndex(value, arr) {
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] === value) {
-            return i;
-        }
-    }
-    return -1; //to handle the case where the value doesn't exist
-}
-
 class ShoppingCart extends React.Component {
     constructor(props) {
         super(props);
@@ -45,43 +36,60 @@ class ShoppingCart extends React.Component {
     // IGNORE BAD CODE BELOW :P
 
     // Removing an item from ze basket
-    handleRemove(product, event) {
-
-        //var products = JSON.parse(localStorage.getItem('shoppingCart'));
-        //var index = getIndex(product, products);
-        //products.items.splice(index, 1);
-        //// update shopping cart
-        //localStorage.setItem('shoppingCart', JSON.stringify(products));
-        //// refresh
-        //history.push("/checkout");
+    handleRemove(id, event) {
+        var products = JSON.parse(localStorage.getItem('shoppingCart'));
+        
+        for (var i = 0; i < products.items.length; i++) {
+            if (products.items[i].id === id) {
+                products.items.splice(i, 1);
+            }
+        }
+        // update shopping cart
+        localStorage.setItem('shoppingCart', JSON.stringify(products));
+        // refresh
+        history.push("/checkout");
     }
 
     // Subtracting quantity for an item from ze basket
-    handleSubtract(product, event) {
-        //var products = JSON.parse(localStorage.getItem('shoppingCart'));
-        //// subtract one
-        //products.items[index].amount -= 1;
-        //
-        //// if zero, remove it
-        //if (products.items[index].amount <= 0) {
-        //    products.items.splice(index, 1);
-        //}
-        //// update shopping cart
-        //localStorage.setItem('shoppingCart', JSON.stringify(products));
-        //// refresh
-        //history.push("/checkout");
+    handleSubtract(id, event) {
+        var products = JSON.parse(localStorage.getItem('shoppingCart'));
+        
+        for (var i = 0; i < products.items.length; i++) {
+            if (products.items[i].id === id) {
+                console.log(id);
+                // subtract one
+                products.items[i].amount -= 1;
+        
+                // if zero, remove it
+                if (products.items[i].amount <= 0) {
+                    products.items.splice(i, 1);
+                }
+            }
+        }
+        
+        // update shopping cart
+        localStorage.setItem('shoppingCart', JSON.stringify(products));
+        // refresh
+        history.push("/checkout");
     }
 
     // Adding quantity
-    handleAdd(product, event) {
-        //console.log("Index:" + index);
-        //var products = JSON.parse(localStorage.getItem('shoppingCart'));
-        //// add one
-        //products.items[index].amount += 1;
-        //// update shopping cart
-        //localStorage.setItem('shoppingCart', JSON.stringify(products));
-        //// refresh
-        //history.push("/checkout");
+    handleAdd(id, event) {
+        var products = JSON.parse(localStorage.getItem('shoppingCart'));
+
+        console.log(products.items.length);
+        for (var i = 0; i < products.items.length; i++) {
+            console.log(i);
+            if (products.items[i].id === id) {
+                console.log("ADD");
+                // add one
+                products.items[i].amount += 1;
+            }
+        }
+        // update shopping cart
+        localStorage.setItem('shoppingCart', JSON.stringify(products));
+        // refresh
+        history.push("/checkout");
     }
 
     render() {
@@ -102,24 +110,24 @@ class ShoppingCart extends React.Component {
                         {products.items.length == 0 && <h2>Your shopping cart is empty.</h2>}
                         {products.items.map((product, index) =>
                             <div className="product row" key={index}>
-                                <div className="imageColumn col-md-4" key={index}>
+                                <div className="imageColumn col-md-4">
                                     <Link to={`/product/${product.id}`}>
                                         <img src={product.imageUrl} />
                                     </Link>
                                 </div>
-                                <div className="productColumn col-md-4" key={index}>
+                                <div className="productColumn col-md-4">
                                     <Link to={`/product/${product.id}`}>
                                         <h4>{product.name}</h4>
                                     </Link>
                                     <h2>{product.price},-</h2>
                                 <p>Quantity:
-                                <button class="btn btn-sm" onClick={this.handleAdd(this, index)}>-</button>
+                                <button className="btn btn-sm" onClick={this.handleSubtract.bind(this, product.id)}>-</button>
                                     {product.amount}
-                                    <button class="btn btn-sm" onClick={this.handleSubtract(this, index)}>+</button>
+                                    <button className="btn btn-sm" onClick={this.handleAdd.bind(this, product.id)}>+</button>
                                 </p>
                                 </div>
                             <div className="actionsColumn col-md-4">
-                                <button className="btn btn-danger" onClick={this.handleRemove(this, index)}>Remove</button>
+                                <button className="btn btn-danger" onClick={this.handleRemove.bind(this, product.id)}>Remove</button>
                                 <button className="btn btn-primary">Add to wishlist</button>
                             </div>
                             </div>
