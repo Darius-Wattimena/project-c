d8088a3e9e886f4ffe8926abf78211854cafc284
DESKTOP-QB85EGP\jesse <0945181@hr.nl>
2018-10-08 13:06:19 +0200

    Made horizontal/vertical product Listing

--- ProjectC/ClientApp/src/ProductPage/ProductsPage.jsx
+++ ProjectC/ClientApp/src/ProductPage/ProductsPage.jsx
@@ -6,34 +6,104 @@ import { connect } from 'react-redux';
 import { productActions } from '../_actions';
 
 import '../styling/ProductListingStyle.css';
+import { history } from '../_helpers';
+
+var vertical = false;
+
+function onClick(e) {
+    vertical = !vertical;
+    history.push("/products");
+}
+
+function Listing(props) {
+    const products = props.products;
+    const vertical = props.vertical
+    if (vertical) {
+        return (<VerticalListing products={products} />);
+    }
+    else {
+        return (<HorizontalListing products={products} />);
+    }
+}
+
+function CartButton() {
+    return (
+        <button class="btn cartbutton">Add to cart</button>
+    );
+}
+
+function HorizontalListing(props) {
+    const products = props.products;
+    return (
+        <div>
+            {products.map((product, index) =>
+                <div className="product-item">
+                    <Link to={`/product/${product.id}`}>
+                        <img src={product.imageUrl}></img>
+                        <h4>{product.name}</h4>
+                    </Link>
+                    <h3>{product.price},-</h3>
+                    <CartButton />
+                </div>
+            )}
+        </div>
+    );
+}
+
+function VerticalListing(props) {
+    const products = props.products;
+    return (
+        <div>
+            {products.map((product, index) =>
+                <div className="product-itemv row">
+                    <div className="col-sm-5">
+                        <Link to={`/product/${product.id}`}>
+                            <img src={product.imageUrl}></img>
+                        </Link>
+                    </div>
+                    <div className="col-sm-7">
+                        <h4>{product.name}</h4>
+                        <p>stock: {product.stock}</p>
+                        <h3>{product.price},-</h3>
+                        <CartButton />
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
 
 //base class
 class ProductPage extends React.Component {
+
+
     componentDidMount() {
         this.props.dispatch(productActions.getAll());
     }
-   
+
     render() {
         console.log(this.props);
         const { products } = this.props;
         return (
-            <div>
-            {products.loading && <em>Loading products...</em>}
-            {products.error && <span className="text-danger">ERROR: {products.error}</span>}
-            {products.items &&
-                <div>
-                {products.items.map((product, index) => 
-                    <div className="product-item">
-                        <Link to={`/product/${product.id}`}>
-                            <img src={product.imageUrl}></img>
-                            <h4>{product.name}</h4>
-                        </Link>
-                        <p>stock: {product.stock}</p>
-                        <h3>{product.price},-</h3>
-                    </div>
-                )}
+            <div className="row">
+                <div class="filters-container col-sm-3">
+                    <nav class="navbar navbar-dark bg-info">
+                        <a class="nav-link">Filters</a>
+                    </nav>
                 </div>
-        }
+                <div class="products-container col-sm-9">
+                    <nav class="navbar navbar-dark bg-info">
+                        <ul class="navbar-nav">
+                            <li class="nav-right active">
+                                <a class="nav-link" onClick={onClick}>Switch View</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    {products.loading && <em>Loading products...</em>}
+                    {products.error && <span className="text-danger">ERROR: {products.error}</span>}
+                    {products.items && <Listing products={products.items} vertical={vertical} />}
+                </div>
             </div>
         );
     }
