1b51f9b32a5194ac3894ef0a20ca010052cd5d43
Beau Casemie <0954603@hr.nl>
2018-10-14 03:18:21 +0200

    added some comments to shopping cart reducer

--- ProjectC/ClientApp/src/_reducers/shoppingCart.reducer.js
+++ ProjectC/ClientApp/src/_reducers/shoppingCart.reducer.js
@@ -9,11 +9,15 @@ export function shoppingCart(state = initialState, action) {
     switch (action.type) {
         case shoppingCartConstants.ADD_PRODUCT:
             return {
+                // Whatever was in the state
                 ...state,
+                // Whatever was in the products array, plus the product to add
+                // TODO: Only increase the amount if the product already exists
                 products: [...state.products, action.product]
              };
 
         case shoppingCartConstants.REMOVE_PRODUCT:
+            // Filter the products to exclude items that contain the id of the product to delete
             const filteredProducts = state.products.filter((item) => item.id !== action.product.id);
             return { ...state, products: filteredProducts }
 
