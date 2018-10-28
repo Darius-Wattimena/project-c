9ed40e867957378f7373ce2c1167125d4086a91d
Beau Casemie <0954603@hr.nl>
2018-10-28 18:53:39 +0100

    Order stuff

---
+++ ProjectC/ClientApp/src/_services/order.service.js
@@ -0,0 +1,40 @@
+﻿import { authHeader, config } from '../_helpers';
+
+export const orderService = {
+    create
+};
+
+// Create a new order to be added to the database
+// Requires a collection of shopping cart items
+function create(shoppingCartItems) {
+    console.log(shoppingCartItems);
+    console.log(JSON.stringify(shoppingCartItems));
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(shoppingCartItems)
+    };
+
+    return fetch(config.apiUrl + '/order/create', requestOptions).then(handleResponse, handleError);
+}
+
+function handleResponse(response) {
+    return new Promise((resolve, reject) => {
+        if (response.ok) {
+            // return json if it was returned in the response
+            var contentType = response.headers.get("content-type");
+            if (contentType && contentType.includes("application/json")) {
+                response.json().then(json => resolve(json));
+            } else {
+                resolve();
+            }
+        } else {
+            // return error message from response body
+            response.text().then(text => reject(text));
+        }
+    });
+}
+
+function handleError(error) {
+    return Promise.reject(error && error.message);
+}
