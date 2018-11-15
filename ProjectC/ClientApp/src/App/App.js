8db8b9951c3bd4305244613888553afeadbe8ba7
Darius Wattimena <dariuswattimena@hotmail.com>
2018-11-16 00:53:46 +0100

    Small fixes + cleanup

--- ProjectC/ClientApp/src/App/App.js
+++ ProjectC/ClientApp/src/App/App.js
@@ -43,9 +43,8 @@ class App extends Component {
     render() {
         const { alert } = this.props;
         return (
-                <div>
-                
-                        <Router history={history}>
+            <div>
+                <Router history={history}>
                     <div>
                         <Route path="/" render={(props) => (!props.location.pathname.startsWith("/adminpanel")) && <Header />} />
                         <Route path="/" render={(props) => (props.location.pathname.startsWith("/adminpanel")) && <AdminPanelHeader />} />
@@ -58,25 +57,24 @@ class App extends Component {
                         <Route path="/adminpanel/sales" component={AdminSales} />
                         <Route path="/adminpanel/coupons" component={AdminCoupons} />
                         <Route path="/adminpanel/reviews" component={AdminReviews} />
-
-                                <div className="container">
-                                    <div className="col-sm-8 col-sm-offset-2">
-                                        {alert.message &&
-                                            <div className={`alert ${alert.type}`}>{alert.message}</div>
-                                        }
-                                        </div>
-                                        <PrivateRoute exact path="/" component={HomePage} />
-                                        <Route path="/home" component={HomePage} />
-                                        <Route path="/register" component={RegisterPage} />
-                                        <Route path="/products" component={ProductPage} />
-                                        <Route path="/product/:id" component={SingleProductPage} />
-                                        <Route path="/user/edit/:id" component={EditUserPage} />
-                                        <Route path="/checkout" component={ShoppingCart} />
-                                        <Route path="/login" component={LoginPage} />
-                                        <Route path="/order" component={OrderPage}/>
-                                </div>
+                        <div className="container">
+                            <div className="col-sm-8 col-sm-offset-2">
+                                {alert.message &&
+                                    <div className={`alert ${alert.type}`}>{alert.message}</div>
+                                }
                             </div>
-                    </Router>
+                            <PrivateRoute exact path="/" component={HomePage} />
+                            <Route path="/home" component={HomePage} />
+                            <Route path="/register" component={RegisterPage} />
+                            <Route path="/products" component={ProductPage} />
+                            <Route path="/product/:id" component={SingleProductPage} />
+                            <Route path="/user/edit/:id" component={EditUserPage} />
+                            <Route path="/checkout" component={ShoppingCart} />
+                            <Route path="/login" component={LoginPage} />
+                            <Route path="/order" component={OrderPage}/>
+                        </div>
+                    </div>
+                </Router>
             </div>
         );
     }
