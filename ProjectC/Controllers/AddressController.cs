38e4226bd1379f47b067b0316aa2f876a0a516cc
Darius Wattimena <dariuswattimena@hotmail.com>
2018-11-16 12:36:24 +0100

    Small fixes in all controllers

--- ProjectC/Controllers/AddressController.cs
+++ ProjectC/Controllers/AddressController.cs
@@ -35,7 +35,7 @@ namespace ProjectC.Controllers
         [HttpPut("{id}")]
         public override IActionResult Update(int id, [FromBody] Address input)
         {
-            return InnerSave(input);
+            return InnerSave(input, id);
         }
 
         [HttpDelete("{id}")]
--- ProjectC/Controllers/ProductController.cs
+++ ProjectC/Controllers/ProductController.cs
@@ -81,7 +81,7 @@ namespace ProjectC.Controllers
         [HttpPut("{id}")]
         public override IActionResult Update(int id, [FromBody] Product input)
         {
-            return InnerSave(input);
+            return InnerSave(input, id);
         }
 
         [HttpDelete("{id}")]
