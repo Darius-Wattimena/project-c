2264d103739df77b7cdeadc8cdaf11e61d0eb19b
Beau Casemie <0954603@hr.nl>
2018-12-09 23:37:13 +0100

    Stock message

--- ProjectC/ClientApp/src/SingleProductPage/SingleProductPage.jsx
+++ ProjectC/ClientApp/src/SingleProductPage/SingleProductPage.jsx
@@ -19,7 +19,7 @@ class SingleProductPage extends React.Component {
         console.log("Adding product " + product.name + " to the shopping basket");
         this.props.addProduct(product);
     }
-    
+
     render() {
         const { product } = this.props;
         return (
@@ -37,38 +37,69 @@ class SingleProductPage extends React.Component {
                             <div className="row">
                                 <div className="col-md-5">
                                     <div className="img-container">
-                                        <img src={product.items.imageUrl} alt=""/>
+                                        <img src={product.items.imageUrl} alt="" />
                                     </div>
                                 </div>
                                 <div className="col-md-7">
                                     <h2>{product.items.name}</h2>
                                     <h3>{product.items.price},-</h3>
-                                <div className="button-group">
-                                    <button disabled={(this.props.shoppingCart.adding && this.props.shoppingCart.adding.productId === product.items.id
-                                    )}
-                                        className="btn btn-success" onClick={this.handleAdd.bind(this, product.items)}>Add to cart</button>
+                                    {
+                                        product.items.stock > 0
+                                            ?
+                                            product.items.stock > 5 // In stock and not low
+                                                ?
+                                                <div className="product-stock text-success">
+                                                    In stock &nbsp;
+                                                <i class="fas fa-dolly"></i>
+                                                </div>
+                                                : // In stock but low stock
+                                                <div>
+                                                    <div className="product-stock text-warning">
+                                                        Limited stock &nbsp;
+                                                     <i class="fas fa-people-carry"></i>
+                                                    </div>
+                                                    <br />
+                                                    <small>Only {product.items.stock} left. Order now!</small>
+                                                </div>
+                                            : // Not in stock
+                                            <div>
+                                            <div className="product-stock text-danger">
+                                                Currently not in stock
+                                                     <i class="fas fa-skull-crossbones"></i>
+                                                </div>
+                                                <br />
+                                                <small>You can still order this product but please take extra delivery time into account.</small>
+                                            </div>
+                                    }
+                                    <div className="button-group">
+                                        <button disabled={(this.props.shoppingCart.adding && this.props.shoppingCart.adding.productId === product.items.id
+                                        )}
+                                            className="btn btn-success" onClick={this.handleAdd.bind(this, product.items)}>Add to cart</button>
                                         <button className="btn btn-info">Add to wishlist</button>
                                     </div>
                                     <p>{product.items.description}</p>
                                 </div>
 
-                                {product.items.specifications &&
-                                <table class="table table-hover" style={{ margin: 1 + "em" }}>
-                                    <thead class="thead-dark">
-                                        <tr>
-                                            <th scope="col">#</th>
-                                            <th scope="col">Specificaties</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                    {product.items.specifications.map((spec, index) =>
-                                        <tr>
-                                            <td scope="row">{spec.name}</td>
-                                            <td>{spec.value}</td>
-                                        </tr>
-                                    )}
-                                    </tbody>
-                                </table>
+
+                                {
+                                    // Specifications
+                                    product.items.specifications &&
+                                    <table class="table table-hover" style={{ margin: 1 + "em" }}>
+                                        <thead class="thead-dark">
+                                            <tr>
+                                                <th scope="col">#</th>
+                                                <th scope="col">Specificaties</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {product.items.specifications.map((spec, index) =>
+                                                <tr>
+                                                    <td scope="row">{spec.name}</td>
+                                                    <td>{spec.value}</td>
+                                                </tr>
+                                            )}
+                                        </tbody>
+                                    </table>
                                 }
                             </div>
                         </div>
