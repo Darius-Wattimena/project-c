acc9421d448400f5c5d5820c8261876100a61184
Darius Wattimena <dariuswattimena@hotmail.com>
2018-12-12 16:51:59 +0100

    Small deletion + testing GPG signed commits

--- ProjectC/ClientApp/src/AdminStatistics/AdminStatistics.jsx
+++ ProjectC/ClientApp/src/AdminStatistics/AdminStatistics.jsx
@@ -3,16 +3,6 @@ import { connect } from 'react-redux';
 import { AreaChart, Tooltip, Area, CartesianGrid, XAxis, YAxis } from 'recharts';
 import { statisticsActions } from '../_actions';
 
-const data = [
-    { name: 'Page A', uv: 4000 },
-    { name: 'Page B', uv: 3000 },
-    { name: 'Page C', uv: 2000 },
-    { name: 'Page D', uv: 2780 },
-    { name: 'Page E', uv: 1890 },
-    { name: 'Page F', uv: 2390 },
-    { name: 'Page G', uv: 3490 }
-];
-
 class AdminStatistics extends React.Component {
     constructor(props) {
         super(props);
