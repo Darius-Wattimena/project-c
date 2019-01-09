43c2251415c4690b84a316bb1023ced8d4569f01
Darius Wattimena <dariuswattimena@hotmail.com>
2019-01-09 19:52:33 +0100

    Fixed styling on statistics page

--- ProjectC/ClientApp/src/AdminStatistics/Helpers/AdminStatisticsTopPanel.jsx
+++ ProjectC/ClientApp/src/AdminStatistics/Helpers/AdminStatisticsTopPanel.jsx
@@ -21,15 +21,15 @@ class AdminStatisticsTopPanel extends React.Component {
         return (
             <div>
             {statistics.data &&
-                <div className="row">
-                    <div className="col admin-statistics-panel text-center">
+                <div className="row admin-statistics-top-row">
+                    <div className="col admin-statistics-panel admin-statistics-top-panel text-center">
                         <h4>Total Users</h4>
                         <h5><i className="fas fa-users"/> {statistics.data.totalUsers}</h5>
                     </div>
-                    <div className="col admin-statistics-panel">
+                    <div className="col admin-statistics-panel admin-statistics-top-panel">
                             
                     </div>
-                    <div className="col admin-statistics-panel">
+                    <div className="col admin-statistics-panel admin-statistics-top-panel">
 
                     </div>
                 </div>
