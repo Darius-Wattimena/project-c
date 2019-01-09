bbf9cb4488178f811053e87aaa8467ae589c8399
Darius Wattimena <dariuswattimena@hotmail.com>
2019-01-09 22:04:39 +0100

    Added couple test methods

---
+++ UnitTestProjectC/Database/Daos/UnitTestUserDao.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectC.Database.Core;
+using ProjectC.Database.Daos;
+using ProjectC.Database.Entities;
+
+namespace UnitTestProjectC.Database.Daos
+{
+    [TestClass]
+    public class UnitTestUserDao
+    {
+        DaoManager daoManager = DaoManager.Get(UnitTestDatabaseContext.Get());
+
+        [TestMethod]
+        public void FindAllUsers_WithTheUserDao_AndReturnsAListOfUsers()
+        {
+            UserDao userDao = daoManager.UserDao;
+            var listOfUsers = userDao.FindAll();
+            Assert.IsInstanceOfType(listOfUsers, typeof(List<User>));
+        }
+
+        [TestMethod]
+        public void FindUserWithId_UsingUserId5_IsFirstnameEqualToBeau()
+        {
+            UserDao userDao = daoManager.UserDao;
+            User user = userDao.Find(5);
+            Assert.AreEqual(user.Firstname, "Beau");
+        }
+
+        [TestMethod]
+        public void FindUserWithId_UsingUserId3_IsUserActive()
+        {
+            UserDao userDao = daoManager.UserDao;
+            User user = userDao.Find(3);
+            Assert.AreEqual(user.Active, 1);
+        }
+    }
+}
--- UnitTestProjectC/Database/UnitTestDatabaseContext.cs
+++ UnitTestProjectC/Database/UnitTestDatabaseContext.cs
@@ -1,14 +1,27 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectC.Database;
 
 namespace UnitTestProjectC.Database
 {
     [TestClass]
     public class UnitTestDatabaseContext
     {
+        private static DatabaseContext _instance;
+
+        private static string ConnectionString =
+            "server=c-mobile-mysqldbserver.mysql.database.azure.com;port=3306;database=project5;user=mysqldbuser@c-mobile-mysqldbserver;password=dXd+fT+TM$g5EeVgHQcj2j$F;sslMode=none;";
+
+        public static DatabaseContext Get()
+        {
+            return _instance ?? (_instance = new DatabaseContext(ConnectionString));
+        }
+
         [TestMethod]
         public void SetupDatabaseContext_WithGivenConnectionString()
         {
-
+            DatabaseContext context = new DatabaseContext(ConnectionString);
+            bool hasDatabaseContextConnectionString = context.ConnectionString.Equals(ConnectionString);
+            Assert.IsTrue(hasDatabaseContextConnectionString);
         }
     }
 }
