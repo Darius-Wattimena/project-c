fb5892e8145054b70138b2c53862f3175e88f697
Darius Wattimena <dariuswattimena@hotmail.com>
2019-01-11 14:33:17 +0100

    Added test httpcontext for UnitTestProductController

--- UnitTestProjectC/Controllers/UnitTestProductController.cs
+++ UnitTestProjectC/Controllers/UnitTestProductController.cs
@@ -1,10 +1,61 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Reflection;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ProjectC.Controllers;
+using ProjectC.Database;
+using ProjectC.Database.Core;
+using ProjectC.Database.Entities;
+using UnitTestProjectC.Database;
 
 namespace UnitTestProjectC.Controllers
 {
     [TestClass]
     public class UnitTestProductController
     {
+        public ProductController Controller;
         
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var serviceCollection = new ServiceCollection()
+                .AddLogging();
+            serviceCollection.Add(new ServiceDescriptor(typeof(DatabaseContext), 
+                UnitTestDatabaseContext.Get()));
+            serviceCollection.Add(new ServiceDescriptor(typeof(DaoManager),
+                DaoManager.Get(UnitTestDatabaseContext.Get())));
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var factory = serviceProvider.GetService<ILoggerFactory>();
+            var logger = factory.CreateLogger<ProductController>();
+            Controller = new ProductController(logger)
+            {
+                ControllerContext = new ControllerContext
+                {
+                    HttpContext = new DefaultHttpContext
+                    {
+                        RequestServices = serviceProvider
+                    }
+                }
+            };
+        }
+
+        [TestMethod]
+        public void GetAllWithSpecifications_ReturningAListOfProducts()
+        {
+            OkObjectResult result = (OkObjectResult) Controller.GetAllWithSpecifications();
+            List<Product> resultItems = (List<Product>) result.Value;
+
+            Assert.IsInstanceOfType(resultItems, typeof(List<Product>));
+        }
     }
 }
