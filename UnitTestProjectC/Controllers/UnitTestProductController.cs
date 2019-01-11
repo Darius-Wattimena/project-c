fd9ca99dc9f71f855e7bde2814b7692909adfe9f
DESKTOP-QB85EGP\jesse <0945181@hr.nl>
2019-01-11 17:18:21 +0100

    Unittest ProductController aangepast

--- UnitTestProjectC/Controllers/UnitTestProductController.cs
+++ UnitTestProjectC/Controllers/UnitTestProductController.cs
@@ -16,6 +16,7 @@ using ProjectC.Controllers;
 using ProjectC.Database;
 using ProjectC.Database.Core;
 using ProjectC.Database.Entities;
+using ProjectC.Model;
 using UnitTestProjectC.Database;
 
 namespace UnitTestProjectC.Controllers
@@ -55,7 +56,90 @@ namespace UnitTestProjectC.Controllers
             OkObjectResult result = (OkObjectResult) Controller.GetAllWithSpecifications();
             List<Product> resultItems = (List<Product>) result.Value;
 
+
             Assert.IsInstanceOfType(resultItems, typeof(List<Product>));
         }
+
+        [TestMethod]
+        public void CustomSearch_ReturningSamsungS9()
+        {
+            OkObjectResult result = (OkObjectResult)Controller.CustomSearch("Samsung S9");
+            List<Product> resultItems = (List<Product>)result.Value;
+            var Phone = resultItems[0].Id;
+
+            Assert.AreEqual(4, Phone);
+        }
+
+        [TestMethod]
+        public void GetWithSpecificationsById_ReturnAObjectWithSpecifications()
+        {
+            OkObjectResult result = (OkObjectResult)Controller.GetWithSpecifications(4);
+            Product resultItems = (Product)result.Value;
+            var PhoneSpecifications = resultItems.Specifications;
+
+            Assert.IsNotNull(PhoneSpecifications);
+        }
+
+        [TestMethod]
+        public void GetAllWithoutSpecifications_ReturnProductsWithoutSpecifications()
+        {
+            OkObjectResult result = (OkObjectResult)Controller.GetAllWithoutSpecifications();
+            List<Product> resultItems = (List<Product>)result.Value;
+            var PhoneSpecifications = resultItems[0].Specifications;
+
+            Assert.IsNull(PhoneSpecifications);
+        }
+
+        [TestMethod]
+        public void GetAllWithoutSpecifications_ReturnListOfProducts()
+        {
+            OkObjectResult result = (OkObjectResult)Controller.GetAllWithoutSpecifications();
+            List<Product> resultItems = (List<Product>)result.Value;
+
+            Assert.IsInstanceOfType(resultItems, typeof(List<Product>));
+        }
+
+        [TestMethod]
+        public void UpdateWithSpecificationsTest()
+        {
+            ProductSpecificationsModel mock = new ProductSpecificationsModel();
+            OkObjectResult result = (OkObjectResult)Controller.GetWithSpecifications(4);
+            Product resultItems = (Product)result.Value;
+            Specification[] specifications = resultItems.Specifications.ToArray();
+            mock.Product = resultItems;
+            mock.Specifications = specifications;
+
+            OkResult resulttest = (OkResult) Controller.UpdateWithSpecifications(mock);
+
+            Assert.IsInstanceOfType(resulttest, typeof(OkResult));
+        }
+
+        [TestMethod]
+        public void ChangeStockTest()
+        {
+            ProductChangeStockModel mock = new ProductChangeStockModel();
+            OkObjectResult result = (OkObjectResult)Controller.GetWithSpecifications(4);
+            Product resultItems = (Product)result.Value;
+            Specification[] specifications = resultItems.Specifications.ToArray();
+            mock.Product = resultItems;
+            mock.NewStock = resultItems.Stock;
+
+            OkResult resulttest = (OkResult)Controller.ChangeStock(mock);
+
+            Assert.IsInstanceOfType(resulttest, typeof(OkResult));
+        }
+
+        [TestMethod]
+        public void UpdateProduct()
+        {
+            ProductChangeStockModel mock = new ProductChangeStockModel();
+            OkObjectResult result = (OkObjectResult)Controller.GetWithSpecifications(4);
+            Product resultItems = (Product)result.Value;
+
+            OkObjectResult resulttest = (OkObjectResult)Controller.Update(resultItems.Id, resultItems);
+
+            Assert.IsInstanceOfType(resulttest, typeof(OkObjectResult));
+        }
+
     }
 }
